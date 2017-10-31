4ebc45462bf9d49ffece82f8ad12ceb2cfa46a0a
sainiavtar <sainiavtar95@gmail.com>
2017-10-31 16:32:43 +0530

    depricate

--- conn.php
+++
@@ -1,5 +0,0 @@
-<?php
-$con	=	mysql_connect("localhost","gfd_u","gfd_password1") or die("Unable to connect to MongoDB");
-$db		=	@mysql_select_db("getfreedash",$con) or die("Unable to select MongoDB database");
-
-?>
